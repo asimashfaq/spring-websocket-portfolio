53d62e6c59d3d4a59e9985d5ebc0c6c90f1803a0
AsimAshfaq <asimizb4@gmail.com>
2016-10-04 07:26:55 +0500

    Send Connect and Disconnect Signal From Spring RabbitMQ to MQTT server

--- src/main/java/org/springframework/samples/portfolio/config/StompConnectedEvent.java
+++ src/main/java/org/springframework/samples/portfolio/config/StompConnectedEvent.java
@@ -2,6 +2,10 @@ package org.springframework.samples.portfolio.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +20,41 @@ import org.springframework.web.socket.messaging.SessionConnectedEvent;
 @ComponentScan("org.springframework.samples")
 @EnableWebSocketMessageBroker
 public class StompConnectedEvent implements ApplicationListener<SessionConnectedEvent> {
-	 
+    String topic        = "asim";
+    String content      = "Conntected ";
+    int qos             = 0;
+    String broker       = "tcp://50.116.63.192:1883";
+    String clientId     = "JavaSample";
+    MqttClient sampleClient;
     private final Log logger = LogFactory.getLog(SessionConnectedEvent.class);
  
     public void onApplicationEvent(SessionConnectedEvent event) {
         StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
- 
+
+        try {
+            content = content +  sha.getSessionId();
+
+            MqttClient sampleClient = new MqttClient(broker, clientId);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
        // String  company = sha.getNativeHeader("company").get(0);
         logger.debug("Connect event  ************************************************************* [sessionId: " + sha.getSessionId() +"; company:  ]");
     }
--- src/main/java/org/springframework/samples/portfolio/config/StompDisconnectEvent.java
+++ src/main/java/org/springframework/samples/portfolio/config/StompDisconnectEvent.java
@@ -11,18 +11,56 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 
 @Configuration
 @EnableScheduling
 @ComponentScan("org.springframework.samples")
 @EnableWebSocketMessageBroker
 public class StompDisconnectEvent implements ApplicationListener<SessionDisconnectEvent> {
-	 
+
+    String topic        = "asim";
+    String content      = "Disconntect ";
+    int qos             = 0;
+    String broker       = "tcp://50.116.63.192:1883";
+    String clientId     = "JavaSample";
+    MqttClient sampleClient;
+
+
     private final Log logger = LogFactory.getLog(SessionDisconnectEvent.class);
  
     public void onApplicationEvent(SessionDisconnectEvent event) {
         StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
- 
+
+        try {
+            content = content +  sha.getSessionId();
+
+            MqttClient sampleClient = new MqttClient(broker, clientId);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
        // String  company = sha.getNativeHeader("company").get(0);
         logger.debug("Connect event xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx [sessionId: " + sha.getSessionId() +"; company:  ]");
     }
